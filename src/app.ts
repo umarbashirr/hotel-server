4dc1c32c2cf557be5c43d65b53ebcded10c0cd01
Umar Bashir <umarbashir93@outlook.com>
2024-02-13 00:38:23 +0530

    Add @types/cors and cors package

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-
+import cors from 'cors';
 const app = express();
 
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
